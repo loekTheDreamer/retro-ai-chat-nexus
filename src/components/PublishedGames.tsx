33c08c12c2b39be69789ce6a53606b1887a8a341
Luke Carelsen <luke.carelsen@me.com>
2025-05-02 11:56:53 +0200

    style almost

--- src/components/PublishedGames.tsx
+++ src/components/PublishedGames.tsx
@@ -116,35 +116,36 @@ const PublishedGames = () => {
   return (
     <div className='w-full py-6 px-4 md:px-6'>
       {/* <h2 className="text-3xl font-bold mb-6 text-neoplay-purple">Published Games</h2> */}
-      <div className='grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-6 w-full'>
+      <div className='grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] gap-3 lg:gap-6 w-full'>
         {games.map((game) => (
           <div
             key={game.id}
-            className='border-2 border-neoplay-green p-4 hover:bg-neoplay-gray cursor-pointer transition-colors w-full'>
-            <h2 className='font-pixel text-lg'>{game.name}</h2>
+            className='border-2 border-neoplay-green p-4 hover:bg-neoplay-gray cursor-pointer transition-colors w-[250px] h-[400px] mx-auto'
+          >
+            <h2 className='font-pixel text-lg truncate'>{game.name}</h2>
 
-            <div className='h-48 bg-neoplay-gray mt-2 flex items-center justify-center'>
+            <div className='h-48 bg-neoplay-gray mt-2 flex items-center justify-center w-full overflow-hidden'>
               <img
                 // src={`${VITE_SEVALLA_BUCKET_PUBLIC_DOMAIN}/published/${game.id}/img/coverImage.png`}
                 src={`https://${VITE_SEVALLA_BUCKET_PUBLIC_DOMAIN}/published/${
                   game.id
                 }/img/coverImage.png?${Date.now()}`}
                 alt='Game Cover'
-                className='max-h-48 object-contain'
+                className='h-full w-full object-cover'
               />
 
-              {/* <span className='font-pixel text-sm'>GAME PREVIEW</span> */}
+              {/* <span className="font-pixel text-sm">GAME PREVIEW</span> */}
             </div>
 
-            <p className='mt-3 text-sm'>{game.description}</p>
+            <p className='mt-3 text-sm line-clamp-3'>{game.description}</p>
 
-            <div className='flex justify-between items-center mt-4 text-xs'>
+            <div className='flex justify-between items-center mt-2 text-xs'>
               <span>
                 BY: {game.publisher.walletAddress.slice(2, 6)}...
                 {game.publisher.walletAddress.slice(-4)}
               </span>
             </div>
-            <div className='flex justify-between items-center mt-4 text-xs'>
+            <div className='flex justify-between items-center mt-2 text-xs'>
               <span className='inline-flex items-center gap-1'>
                 <Play
                   className='w-4 h-4 text-neoplay-green'
